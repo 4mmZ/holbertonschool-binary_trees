bca255b1437cc7373a6248cd18bcbb1d28ee7545
Andres <andresmaxi111014@gmail.com>
2023-01-12 11:13:11 -0800

    betty issues and coding style fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ *binary_tree_node - Function that creates a binary tree node
+ *@parent: Is a pointer to the parent node of the node to create
+ *@value: Is the value to put in the new node
+ *Return: The new node or NULL on failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root = malloc(sizeof(binary_tree_t));
@@ -7,12 +13,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!root)
 	{
 		free(root);
-		return(NULL);
+		return (NULL);
 	}
 	if (!value)
 	{
 		free(root);
-		return(NULL);
+		return (NULL);
 	}
 	root->parent = parent;
 	root->n = value;
