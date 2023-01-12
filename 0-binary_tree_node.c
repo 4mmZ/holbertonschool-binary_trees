7072e2856da75a717431525cf442d9672854ea91
Andres <andresmaxi111014@gmail.com>
2023-01-12 11:09:43 -0800

    fix malloc returns for task 0 part 4

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,6 +4,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root = malloc(sizeof(binary_tree_t));
 
+	if (!root)
+	{
+		free(root);
+		return(NULL);
+	}
 	if (!value)
 	{
 		free(root);
