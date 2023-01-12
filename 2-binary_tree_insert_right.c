181347ae747707aff4a9a9cadec67d3153ecb730
Andres <andresmaxi111014@gmail.com>
2023-01-12 11:38:13 -0800

    task 1 betty fix, full task 2 and header files

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_insert_left -  function that inserts a node
+ *binary_tree_insert_right -  function that inserts a node
  * as the left-child of another node
  *@parent: Is a pointer to the node to insert the left-child in
  *@value: Is the value to store in the new node
  *Return: A pointer ti the create node, or NULL on failure
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root = NULL;
 
@@ -21,12 +21,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	root->parent = parent;
 	root->n = value;
-	if (parent->left)
+	if (parent->right)
 	{
-		root->left = parent->left;
-		parent->left->parent = root;
+		root->right = parent->right;
+		parent->right->parent = root;
 	}
-	parent->left = root;
+	parent->right = root;
 
-	return (parent->left);
+	return (parent->right);
 }
